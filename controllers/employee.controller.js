60af524d658e6843f9199db5e624a7e5f1a735d6
kulaevMagomed94 <kulayev94@mail.ru>
2023-08-08 14:18:22 +0300

    роут мультера

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -22,7 +22,7 @@ module.exports.employeeController = {
       role,
     } = req.body;
 
-    console.log(req.files)
+    console.log(req.files);
     try {
       const employee = await Employee.create({
         email,
@@ -49,19 +49,39 @@ module.exports.employeeController = {
       res.json(error.message("Ошибка в deleteEmployee"));
     }
   },
-  patchEmployee: async (req, res)=>{
-    try{
-        const employee = await Employee.findByIdAndUpdate(req.params.id,{
-        email:req.body.email,
-        firstName:req.body.firstName,
-        secondName:req.body.secondName,
-        category:req.body.category,
-        role:req.body.role,
-        image: req.files.map(item => item.path)
-        })
-        res.json(employee)
-    }catch(error){
-        res.json(error.message('Ошибка в patchEmployee'))
+  patchEmployee: async (req, res) => {
+    const {
+      email,
+      firstName,
+      secondName,
+      login,
+      password,
+      category,
+      task,
+      team,
+      role,
+    } = req.body;
+    try {
+      const employee = await Employee.findByIdAndUpdate(
+        req.params.id,
+        {
+          email,
+          firstName,
+          secondName,
+          login,
+          password,
+          category,
+          task,
+          team,
+          role,
+          image: req.files && req.files.map(item=>path)
+        },
+        
+        { new: true }
+      );
+      return res.json(employee);
+    } catch (error) {
+      res.json(error.message("Ошибка в patchEmployee"));
     }
-  }
+  },
 };
--- routes/employee.route.js
+++ routes/employee.route.js
@@ -3,9 +3,9 @@ const {employeeController}=require('../controllers/employee.controller');
 const router = Router();
 const multerMiddleware = require('../middleware/multer.middleware')
 
+router.patch('/patchEmployees/:id', multerMiddleware.array('image', 5), employeeController.patchEmployee);
 router.get('/getEmployees',employeeController.getEmployee);
 router.post('/addEmployees', multerMiddleware.array('image', 5),employeeController.postEmployee);
 router.delete('/deleteEmployees/:id',employeeController.deleteEmployee);
-router.patch('/patchEmployees/:id',multerMiddleware.array('image', 5),employeeController.patchEmployee);
 
 module.exports = router;
